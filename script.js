d3450d55b66af668480f279cf1b3c870d7d3613c
Rafael Morales <58229065+RafaelDEV-cr@users.noreply.github.com>
2026-04-05 20:01:09 -0300

    refactor: mudança na estrutura do código e algumas melhorias

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//solicita ao usuario o nome do vinho desejada por este
+let nome_vinho = prompt("Qual vinho o senhor deseja?\n1 - Pétrus\n2 - Domaine de La Romanée-Conti\n3 - Sauvignon Del Vale");
 //solicita ao usuario a opcao de vinho desejada por este
 let vinho = prompt("Qual vinho o senhor deseja\n 1 - Tinto\n2 - Branco\n3 - Rose");
 //solicita ao usuario o ano da safra 
@@ -6,12 +8,13 @@ let safra = prompt("De qual ano o senhor deseja a safra?");
 let estoque = prompt("Qual a quantidade em estoque?");
 
 //inserção de variaveis com o prompt sempre retornando em string, tornando necessario converte-los em valor numerico 
+nome_vinho = parseint(nome_vinho);
 vinho = parseInt(vinho);
 safra = parseInt(safra);
 estoque = parseInt(estoque);
 
 // verifica o conjunto valido de opcoes 
-if (vinho < 1 || vinho > 3) {
+if (vinho < 1 || vinho > 3) and (nome_vinho < 1 || nome_vinho > 3 ) {
     // Se a opção for inválida, exibe um alerta avisando o usuário
     alert("Digite uma opção válida");
 }
@@ -22,14 +25,29 @@ else {
 
 // Verifica cada opção escolhida e exibe os dados correspondentes no console
 if (vinho == 1) {
-
-    console.log(`Vinho cadastrado: Tinto\nSafra: ${safra}\nQntd no estoque: ${estoque}`);
+   if (nome_vinho == 1) {
+     console.log(`Vinho cadastrado: Pétrus Tinto\nSafra: ${safra}\nQntd no estoque: ${estoque}`);
+   }
+   else if (nome_vinho == 2) {
+     console.log(`Vinho cadastrado: Domaine de La Romanée-Conti Tinto\nSafra: ${safra}\nQntd no estoque: ${estoque}`);
+   else {
+     console.log(`Vinho cadastrado: Sauvignon Del Vale Tinto\nSafra: ${safra}\nQntd no estoque: ${estoque}`);
 }
 else if (vinho == 2) {
-
-    console.log(`Vinho cadastrado: Branco\nSafra: ${safra}\nQntd no estoque: ${estoque}`);
+   if (nome_vinho == 1) {
+     console.log(`Vinho cadastrado: Pétrus Branco\nSafra: ${safra}\nQntd no estoque: ${estoque}`);
+   }
+   else if (nome_vinho == 2) {
+     console.log(`Vinho cadastrado: Domaine de La Romanée-Conti Branco\nSafra: ${safra}\nQntd no estoque: ${estoque}`);
+   else {
+     console.log(`Vinho cadastrado: Sauvignon Del Vale Branco\nSafra: ${safra}\nQntd no estoque: ${estoque}`);    
 }
 else {
-
-    console.log(`Vinho cadastrado: Rosé\nSafra: ${safra}\nQntd no estoque: ${estoque}`);
+   if (nome_vinho == 1) {
+     console.log(`Vinho cadastrado: Pétrus Rose\nSafra: ${safra}\nQntd no estoque: ${estoque}`);
+   }
+   else if (nome_vinho == 2) {
+     console.log(`Vinho cadastrado: Domaine de La Romanée-Conti Rose\nSafra: ${safra}\nQntd no estoque: ${estoque}`);
+   else {
+     console.log(`Vinho cadastrado: Sauvignon Del Vale Rose\nSafra: ${safra}\nQntd no estoque: ${estoque}`);
 }
